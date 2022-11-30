a4da9862aa0851191ca073c6ba2b074612ee8f21
Ricardo de Genova <ricgnv@gmail.com>
2022-11-30 10:24:02 -0300

    added modules

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,19 +4,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ScheduleModule } from './schedule/schedule.module';
+import { ClassroomModule } from './classroom/classroom.module';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/school'),
     UserModule,
     AuthModule,
-    MongooseModule.forRoot(
-      'mongodb://127.0.0.1:27017/school',
-    ),
+    ScheduleModule,
+    ClassroomModule,
+    TeacherModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-  ],
+  providers: [AppService],
 })
-export class AppModule { }
-
+export class AppModule {}
