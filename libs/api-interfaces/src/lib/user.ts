37e7ff9f14bac24f87c05c2ce140c23183b1ff7a
Ricardo de Genova <ricgnv@gmail.com>
2022-12-20 11:27:27 -0300

    changed role to teacher and isAdmin (for mongoose)

--- libs/api-interfaces/src/lib/user.ts
+++ libs/api-interfaces/src/lib/user.ts
@@ -1,13 +1,11 @@
-import { Admin } from "./admin";
 import { Teacher } from "./teacher";
-
-export type Role = Teacher | Admin;
 
 export interface User {
     id: string;
     name: string;
     email: string;
     password: string;
-    role: Role;
+    teacher: Teacher;
+    isAdmin: boolean;
 }
 
