96a311ba54af202c9a66e8bdb1ffcd02fc1484c5
RicardoDeGenova <ricgnv@gmail.com>
2023-03-02 11:29:09 -0300

    added local settings

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtSecret } from './constants';
+import { jwtSecret } from '@school-nx-monorepo/settings';
 import { JwtStrategy as JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './auth.controller';
